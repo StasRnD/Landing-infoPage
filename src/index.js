9409a485c6b6ddefa48e3381d34fa0aaff168fb2
Stanislav <blackfur4a@mail.ru>
2022-11-07 00:03:39 +0300

    feat: work with localStorage

--- src/index.js
+++ src/index.js
@@ -1,109 +1,43 @@
 import './index.scss';
 import axios from 'axios';
 
-const loadBtn = document.querySelector('.js-load');
-const resultsContainer = document.querySelector('.js-results');
-const searchInput = document.querySelector('.js-input');
-const resultsContainerUsers = document.querySelector('.js-results-users');
-const inputForm = document.querySelector('.input-form-js');
-const validateInputTelText = document.querySelector('.validate-input-tel-js');
-const form = document.querySelector('.form');
+const textArea = document.querySelector('.textarea__input');
+const clearButton = document.querySelector('.clearTextArea');
+const buttonDarkColor = document.querySelector('.searchThemeColor__dark');
+const buttonLightColor = document.querySelector('.searchThemeColor__light');
+const body = document.querySelector('.body');
 
-//Исправлен баг с запросе fetch
-loadBtn.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  const searchValue = searchInput.value.trim().toLowerCase();
-  fetch(`https://api.github.com/users/${searchValue}`)
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error(response.status);
-      }
-    })
-    .then(
-      (data) =>
-        (resultsContainer.innerHTML = `<div class="response-container">
-                <img src="${data.avatar_url}">
-                <p> Имя: <span>${data.name}</span><p>
-                <p> О себе: <span>${data.bio}</span><p>
-                <p> Кол-во репозиториев: <span>${data.public_repos}</span><p>
-            </div>`)
-    )
-    .catch(
-      (error) =>
-        (resultsContainer.innerHTML = `<div class="response-container">
-          <p> Такого пользователя не существует ${error}<p>
-      </div>`)
-    );
-});
-
-//Запрос с помощью axios
-axios
-  .get('https://jsonplaceholder.typicode.com/users')
-  .then((res) => {
-    res.data.map((user) =>
-      resultsContainerUsers.insertAdjacentHTML(
-        'beforeend',
-        `<div class="response-container">
-
-          <p> Имя: <span>${user.name}</span><p>
-          <p> Телефон: <span>${user.phone}</span><p>
-          <p> Cайт: <span>${user.website}</span><p>
-      </div>`
-      )
-    );
-  })
-  .catch(
-    (error) =>
-      (resultsContainerUsers.innerHTML = `<div class="response-container">
-        <p>Не удалось загрузить данные: ${error.message}<p>
-    </div>`)
-  );
-
-//Валидация инпута с помощью с использованием try/catch
-const validate = (evt) => {
-  const value = evt.target.value.trim();
-
-  try {
-    if (value === '') throw new Error('поле не должно быть пустым');
-
-    if (!Number(value))
-      throw new Error('В поле должны быть введены только цифры');
-
-    if (value.length < 5 || value.length > 10)
-      throw new Error('Кол-во символов поля должно быть от 5 до 10 символов');
-
-    validateInputTelText.classList.remove('form__span_active');
-  } catch (err) {
-    validateInputTelText.classList.add('form__span_active');
-    validateInputTelText.textContent = err.message;
+const writeTextfromLocalStorage = () => {
+  textArea.value = localStorage.getItem('textarea');
+  if (localStorage.getItem('darkThemeColor')) {
+    body.classList.add();
   }
 };
 
-inputForm.addEventListener('input', validate);
-form.addEventListener('submit', (evt) => {
-  evt.preventDefault();
+const changeBodyThemeColor = () => {
+  localStorage.getItem('darkThemeColor') === 'true'
+    ? body.classList.add('body_dark')
+    : body.classList.remove('body_dark');
+};
+
+buttonDarkColor.addEventListener('click', () => {
+  localStorage.setItem('darkThemeColor', 'true');
+  changeBodyThemeColor();
 });
 
-//Переписанная функция лоттерие с помощью async/await
-async function lottery() {
-  console.log('Вы начали игру');
-  let promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      Math.random() > 0.5 ? resolve() : reject();
-    }, 1000);
-  });
+buttonLightColor.addEventListener('click', () => {
+  localStorage.setItem('darkThemeColor', 'false');
+  changeBodyThemeColor();
+});
 
-  try {
-    await promise;
-    console.log('Вы выиграли');
-    console.log('Вы заплатили');
-  } catch {
-    console.log('Вы проиграли');
-  } finally {
-    console.log('Игра закончена');
-  }
-}
+clearButton.addEventListener('click', () => {
+  localStorage.removeItem('textarea');
+  writeTextformLocalStorage();
+});
 
-lottery();
+textArea.addEventListener('input', (evt) => {
+  localStorage.setItem('textarea', evt.target.value);
+});
+
+changeBodyThemeColor();
+writeTextfromLocalStorage();
